1e47d7da43a04f794d59a34530f2ca5ce1abb3f2
sorochenkovr <71348466+sorochenkovr@users.noreply.github.com>
2023-03-12 23:01:34 +0300

    Add files via upload

---
+++ 1_labor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labor_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<Action<int, int>, bool> func = (Max) =>
+            {
+                return true;
+            };
+        }
+        static void Max(int x, int y)
+        {
+            Console.WriteLine(x > y);
+        }
+    }
+}
